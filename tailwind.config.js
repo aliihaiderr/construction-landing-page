83d11691170c6e0628550e2d89f6525dd3a0840f
aliihaiderr <alihaidar.dev@gmail.com>
2023-09-04 21:16:25 +0500

    update tailwind config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
       },
     },
     extend: {
+      backgroundImage: {
+        'hero-image': "url('/assets/bg-hero.jpg')",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
