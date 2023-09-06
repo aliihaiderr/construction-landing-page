29eb7e8571e37219fc48c2916ba4dab34e76e401
aliihaiderr <alihaidar.dev@gmail.com>
2023-09-06 21:32:25 +0500

    project card component

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+import { Button } from './ui/button'
+
+
+const ProjectCard = () => {
+  return (
+    <div className=''>
+        <div>
+            <Image src={'/assets/bg-detail.jpg'} alt='projectImg' width={500} height={700} />
+        </div>
+        <div className='mt-4 border p-6 border-gray-400 flex flex-col space-y-4'>
+            <h4 className="scroll-m-20 text-lg font-semibold tracking-wide">
+                Project Title
+            </h4>
+            <p className='text-sm'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                Quas laborum asperiores maiores recusandae voluptate qui quasi perspiciatis ipsam eos mollitia.
+            </p>
+            <div>
+                <Button variant="smoutline">VIEW PROJECT</Button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectCard
