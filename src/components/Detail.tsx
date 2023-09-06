b8e4a08cf21957a7efb2358e89b9034737441600
aliihaiderr <alihaidar.dev@gmail.com>
2023-09-06 10:01:58 +0500

    minor update

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -7,9 +7,7 @@ const Detail = () => {
     <div className='flex h-screen'>
 
       {/* Left side Panel */}
-
       <div className='w-2/5 p-12 flex flex-col items-baseline bg-detail-image bg-cover'></div>
-
 
       {/* Right Side Panel  */}
       <div className='w-3/5 bg-gray-200'>
@@ -41,7 +39,6 @@ const Detail = () => {
               <h3 className='scroll-m-20 font-normal text-sm tracking-wider leading-6 transition-colors first:mt-0'>PROJECTS</h3>
             </div>
         </div>
-
       </div>
 
     </div>
