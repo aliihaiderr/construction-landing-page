54df67e2ad393938666be86edadcc87a3f1ffb51
aliihaiderr <alihaidar.dev@gmail.com>
2023-09-06 21:32:02 +0500

    new varient added

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,15 +5,17 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center text-xs tracking-wider font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center text-sm tracking-widest font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-[#F5AA0E] text-primary-foreground hover:bg-[#F5A014]",
+        default: "bg-[#F5AA0E] text-primary-foreground tracking-wider hover:bg-[#F5A014]",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-4 border-gray-700 bg-gray-200 hover:bg-[#E6CF50] hover:text-accent-foreground",
+          "border border-2 border-gray-800 bg-gray-200 hover:bg-gray-300 hover:text-accent-foreground",
+        smoutline:
+          "border border-2 border-gray-400 p-4 font-medium text-xs text-gray-600 bg-gray-200 hover:bg-gray-300 hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
