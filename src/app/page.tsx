4bb6bd72d56baaf4fc056bdb6e66109530a87af2
aliihaiderr <alihaidar.dev@gmail.com>
2023-09-07 21:43:57 +0500

    about component call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/HeroSection"
 import Detail from "@/components/Detail"
 import LatestProjects from "@/components/LatestProjects"
+import About from "@/components/About"
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <Detail />
       <LatestProjects />
+      <About />
     </>
   )
 }
