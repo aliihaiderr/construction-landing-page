f3d6fd0fe373e9bc6e6340e4506754194592a7f2
aliihaiderr <alihaidar.dev@gmail.com>
2023-11-21 01:18:57 +0500

    hero section responsive

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,99 +1,92 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
-import {GiPaintBucket} from "react-icons/gi"
-import {BiSolidBuildingHouse} from "react-icons/bi"
-import {LuLampFloor} from "react-icons/lu"
-import {MdFoundation} from "react-icons/md"
-
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { GiPaintBucket } from "react-icons/gi";
+import { BiSolidBuildingHouse } from "react-icons/bi";
+import { LuLampFloor } from "react-icons/lu";
+import { MdFoundation } from "react-icons/md";
 
 const HeroSection = () => {
   return (
-    <div className='md:flex h-screen'>
-
+    <div className="lg:flex h-screen">
       {/* Left side Panel */}
-      <div className='w-2/3 p-48 flex flex-col items-baseline bg-hero-image bg-cover'>
-        <h2 className="scroll-m-20 pb-2 font-bold tracking-wide leading-8 transition-colors first:mt-0">
-          <span className='text-6xl text-[#F5AA0E]'>Baba Fareed</span> <br/> <span className='md:text-3xl text-white'> Construction & Developers Company</span>
+      <div className="px-8 py-12 max-w-full mx-auto lg:w-2/3 lg:p-48 flex flex-col items-baseline bg-hero-image bg-cover">
+        <h2 className="scroll-m-20 pb-2 font-bold tracking-wide leading-6 transition-colors first:mt-0 lg:leading-8">
+          <span className="text-5xl lg:text-6xl text-[#F5AA0E]">
+            Baba Fareed
+          </span>{" "}
+          <br />{" "}
+          <span className="text-xl lg:text-3xl text-white">
+            {" "}
+            Construction & Developers Company
+          </span>
         </h2>
-        <p className="leading-7 [&:not(:first-child)]:mt-2 mb-6 text-gray-200 font-normal tracking-wide">
-          Transforming Visions into Reality, Your Trusted Partner in Construction Excellence.
+        <p className="text-sm leading-5 mt-2 mb-6 text-gray-200 font-normal tracking-wide lg:leading-7 lg:text-base [&:not(:first-child)] ">
+          Transforming Visions into Reality, Your Trusted Partner in
+          Construction Excellence.
         </p>
         <Button>VIEW OUR WORK</Button>
       </div>
 
       {/* Right Side Panel  */}
-      <div className='w-1/3 p-12 flex flex-col items-baseline bg-[#F5AA0E]'>
-        <h2 className="scroll-m-20 pb-6 text-3xl font-extrabold tracking-normal transition-colors first:mt-16">
+      <div className="px-8 py-6 flex justify-center items-center lg:w-1/3 lg:px-16 lg:py-12 flex-col lg:items-baseline bg-[#F5AA0E]">
+        <h2 className="scroll-m-20 pb-6 mt-6 text-3xl font-bold tracking-wide transition-colors lg:first:mt-16">
           Our Services
         </h2>
 
         {/* Design & Build */}
 
-        <div className='flex ml-6 gap-8 mt-10'>
-          <div className='text-6xl font-normal text-white'>
+        <div className="flex ml-6 gap-8 mt-10">
+          <div className="text-6xl font-normal text-white">
             <BiSolidBuildingHouse />
           </div>
           <div>
             <h4 className="scroll-m-20 text-lg font-bold tracking-wide">
               Design & Build
             </h4>
-            <p className="leading-5 [&:not(:first-child)]:mt-3 text-sm text-white">
-            We excel in design and build projects, delivering integrated solutions & construction execution.
+            <p className="leading-5 [&:not(:first-child)]:mt-2 text-xs text-justify text-white">
+              We excel in design and build projects, delivering integrated
+              solutions & construction execution.
             </p>
           </div>
         </div>
 
         {/* Foundation Work */}
 
-        <div className='flex ml-6 gap-8 mt-10'>
-          <div className='text-6xl font-normal text-white'>
+        <div className="flex ml-6 gap-8 mt-10">
+          <div className="text-6xl font-normal text-white">
             <MdFoundation />
           </div>
           <div>
             <h4 className="scroll-m-20 text-lg font-bold tracking-wide">
               Foundation Work
             </h4>
-            <p className="leading-5 [&:not(:first-child)]:mt-3 text-sm text-white">
-            our construction company expertise shines through, ensuring strong, stable, and enduring bases for every project we undertake.
+            <p className="leading-5 [&:not(:first-child)]:mt-2 text-xs text-justify text-white">
+              our construction company expertise shines through, ensuring
+              strong, stable, and enduring bases for every project we undertake.
             </p>
           </div>
         </div>
 
         {/* Renovation */}
 
-        <div className='flex ml-6 gap-8 mt-10'>
-          <div className='text-6xl font-normal text-white'>
+        <div className="flex ml-6 gap-8 mt-10">
+          <div className="text-6xl font-normal text-white">
             <GiPaintBucket />
           </div>
           <div>
             <h4 className="scroll-m-20 text-lg font-bold tracking-wide">
               Renovation
             </h4>
-            <p className="leading-5 [&:not(:first-child)]:mt-3 text-sm text-white"> 
-              Renovation is where our construction company craftsmanship and attention to detail truly transform spaces, breathing new life into existing structures and exceeding our clients expectations.
+            <p className="leading-5 [&:not(:first-child)]:mt-2 text-xs text-justify text-white">
+              Renovation is where our construction company craftsmanship and
+              attention to detail truly transform spaces, breathing new life
+              into existing structures and exceeding our clients expectations.
             </p>
           </div>
         </div>
-
-        {/* Interior & Flooring */}
-
-        {/* <div className='flex ml-6 gap-8 mt-10'>
-          <div className='text-6xl font-normal text-white'>
-            <LuLampFloor />
-          </div>
-          <div>
-            <h4 className="scroll-m-20 text-lg font-bold tracking-wide">
-              Interior & Flooring
-            </h4>
-            <p className="leading-5 [&:not(:first-child)]:mt-3 text-sm text-white">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, ullam.
-            </p>
-          </div>
-        </div> */}
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
