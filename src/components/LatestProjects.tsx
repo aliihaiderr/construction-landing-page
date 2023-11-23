d9b4895920efb9dbf73c942a0aa47028c69f2082
aliihaiderr <alihaidar.dev@gmail.com>
2023-11-23 17:11:28 +0500

    latest project section responsiveness

--- src/components/LatestProjects.tsx
+++ src/components/LatestProjects.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import ProjectCard from './ProjectCard'
-import { Button } from './ui/button'
+import React from "react";
+import ProjectCard from "./ProjectCard";
+import { Button } from "./ui/button";
 
 const LatestProjects = () => {
   return (
-    <div className='p-14'>
-      <div className='flex justify-center'>
-        <h2 className="scroll-m-20 font-bold text-4xl pb-10 text-gray-800 tracking-wide leading-10 transition-colors first:mt-0">
-            Latest Projects
+    <div className="p-14">
+      <div className="flex justify-center">
+        <h2 className="scroll-m-20 font-bold text-3xl lg:text-4xl pb-10 text-gray-800 tracking-wide leading-10 transition-colors first:mt-0">
+          Latest Projects
         </h2>
       </div>
-      <div className='grid grid-cols-3 gap-3'>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-3">
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
       </div>
-      <div className='flex justify-center p-14'>
-            <Button variant='outline'>LEARN MORE</Button>
+      <div className="flex justify-center p-14">
+        <Button variant="outline">LEARN MORE</Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LatestProjects
+export default LatestProjects;
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import Image from 'next/image'
-import { Button } from './ui/button'
-
+import React from "react";
+import Image from "next/image";
+import { Button } from "./ui/button";
 
 const ProjectCard = () => {
   return (
-    <div className=''>
+    <div className="">
+      <div>
+        <Image
+          src={"/assets/bg-detail.jpg"}
+          alt="projectImg"
+          width={500}
+          height={700}
+        />
+      </div>
+      <div className="mt-4 border p-6 border-gray-300 flex flex-col space-y-4">
+        <h4 className="scroll-m-20 text-base lg:text-lg font-semibold tracking-wide">
+          Project Title
+        </h4>
+        <p className=" text-xs text-justify lg:text-sm">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas laborum
+          asperiores maiores recusandae voluptate qui quasi perspiciatis ipsam
+          eos mollitia.
+        </p>
         <div>
-            <Image src={'/assets/bg-detail.jpg'} alt='projectImg' width={500} height={700} />
+          <Button variant="smoutline">VIEW PROJECT</Button>
         </div>
-        <div className='mt-4 border p-6 border-gray-300 flex flex-col space-y-4'>
-            <h4 className="scroll-m-20 text-lg font-semibold tracking-wide">
-                Project Title
-            </h4>
-            <p className='text-sm'>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                Quas laborum asperiores maiores recusandae voluptate qui quasi perspiciatis ipsam eos mollitia.
-            </p>
-            <div>
-                <Button variant="smoutline">VIEW PROJECT</Button>
-            </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
